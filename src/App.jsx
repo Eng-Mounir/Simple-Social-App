dca9aca5498b0976afd4eac8168ef49c1d265cc0
Eng-Mounir <mouniramir39@gmail.com>
2026-02-06 22:56:28 +0200

    Add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-
-import './App.css'
-
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
+import MainLayout from './Layouts/MainLayout';
+import AuthLayout from './Layouts/AuthLayout'; 
+import NewFeed from "./Pages/NewFeed/NewFeed";
+import UserProfile from "./Pages/UserProfile/UserProfile";
+import NotFound  from './Pages/NotFound/NotFound';
+import Registration from './Pages/Auth/Registration/Registration';
+import Login from './Pages/Auth/Login/Login';
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "",
+      element: <MainLayout />,
+      children: [
+        { index: true, element: <Navigate to="/home" /> },
+        { path: "/home", element: <NewFeed /> },
+        { path: "/profile", element: <UserProfile /> },
+        { path: "*", element: <NotFound /> }
+      ]
+    },{
+      path: "",
+      element: <AuthLayout />,
+      children: [
+        { path: "/registration", element: <Registration /> },
+        { path: "/login", element: <Login /> }
+      ]
+    }
+  ]);
 
-  return (
-    <>
-<h1>alo</h1>
-    </>   
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import NavBar from '../componenets/NavBar/NavBar'
+import { Outlet } from 'react-router-dom'
+import Footer from '../componenets/Footer/Footer'
 
 export default function MainLayout() {
   return (
-    <div>MainLayout</div>
+    <>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </>
   )
 }
